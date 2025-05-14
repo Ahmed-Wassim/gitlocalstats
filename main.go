9267a1b4f2b656573ae501bec660f79f0e80707b
Ahmed Wassim <ahmedwassim317@gmail.com>
2025-05-14 21:59:14 +0300

    finish gitlocalstats

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	var folder string
 	var email string
 
-	flag.StringVar(&folder, "folder", "", "add a new folder to scan for Git Repositories")
+	flag.StringVar(&folder, "add", "", "add a new folder to scan for Git Repositories")
 	flag.StringVar(&email, "email", "ahmedwassim317@gmail.com", "email to scan")
 	flag.Parse()
 
